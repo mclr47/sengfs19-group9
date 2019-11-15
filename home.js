f4d9130f63a1d219a41952a7fbbe4632170b2a8c
derekrechtien <dwrvzz@mail.missouri.edu>
2019-11-15 11:49:16 -0600

    graph works but slow

--- home.js
+++ home.js
@@ -1,124 +1,106 @@
+var numbers = [];
+var emails = [];
+
 function getRepoGroupInfo() {
-    
     $.get("http://augur.osshealth.io:5000/api/unstable/repo-groups", function(repodata, status){
         if (status == "success") {
             var repoGroups = repodata;
-            var tops = [];
-            var i = 0;
-            var emails = [];
-            var array = [];
-            var data = new Array();
-            //console.log(typeof(repoGroups));
             //var repogr = repoGroups[3];
             repoGroups.forEach((e) => {
 
                 $.get("http://augur.osshealth.io:5000/api/unstable/repo-groups/" + e.repo_group_id + "/top-committers?threshold=1", function(committerData, status){
 
                     e.committers = committerData;
-                    //e.committers.pop();
-                    console.log(e.committers[0].commits);
-                    data.push(e.committers[0].commits);
-                    emails.push(e.committers[0].email);
-                    tops.push(e.committers[0]);
-                    //createGraph(e.committers[0].commits, e.committers[0].email);
+                    e.committers.pop();
+
                     e.totalCommits = 0;
 
-                    //$("#sectionList").append('<li><a href="#'+ e.repo_group_id + '">'+ e.rg_name + '</a></li>');
+                    //$("#sectionList").append('<li><a href="#'+ repogr.repo_group_id + '">'+ repogr.rg_name + '</a></li>');
 
                     //$("#sectionBody").append('<div id="'+ e.repo_group_id +'"></div>');
                     //$("#" + e.repo_group_id).append('<h1>' + e.rg_name + '</h1>');
                     //$("#" + e.repo_group_id).append('<canvas id="'+ e.repo_group_id +'-canvas"></canvas>');
-
-                    //createGraph(e);
-//                    console.log(typeof(data));
-
+                    $("#" + e.repo_group_id).append('<canvas id="canvas"></canvas>');
+                    var committers = e.committers;
+                    var f = committers[0];
+                    numbers.push(f.commits);
+                    console.log(numbers);
+                    emails.push(f.repo_group_name);
+                    createGraph(e);
                 });
             });
-            //data.toString();
-            //parseInt(data);
-             console.log(tops[0]);
-            var help = new Array(data);
-            //console.log(help[0]["0"]);
-            createGraph(tops);
-            //console.log(data);
-           // console.log(emails);
-            
         }
-    
-    });
+    }); 
+
 }
 
 function createGraph(data) {
+    var id = data.repo_group_id;
+    var committers = data.committers;
+    var otherCommits = 0; //this accounts for any commit
+    var totalCommits = 0;
+    
+    committers.forEach(e => {
+        totalCommits += e.commits;
+    });
+    
+    //var numbers = [];
+    //var emails = [];
     var bgColors = [];
-    console.log(typeof(data));
-    //var data2 = [data];
-            var data2 = [];
-            data.forEach((e) => {
-                 console.log(e);
-                data2.push(e.commits);   
-            });
-    console.log(data2);
-            var borderColors = [];
+    var borderColors = [];
+    var numbers2 = [];
+    var emails2 = [];
+    var e = committers[0];
+    committers.forEach(e => { //separate into two arrays
+        
+        if (e.commits > (totalCommits / 100) * .2) {
+            //numbers.push(e.commits);
+            //emails.push(e.email);
+            
             var r = Math.floor(Math.random() * 255);
             var g = Math.floor(Math.random() * 255);
             var b = Math.floor(Math.random() * 255);
             bgColors.push("rgba(" + r + "," + g + "," + b + ", 0.5)");
             borderColors.push("rgba(" + r + "," + g + "," + b + ", 0.7)");
-            //var ctx = document.getElementById('canvas').getContext('2d');
-			var ctx = document.getElementById("canvas");
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["1", "2", "3", "4", "5", "6", "7", "8"],
-    datasets: [{
-      label: 'Help',
-      data: data2,
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    responsive: false,
-    scales: {
-      xAxes: [{
-        ticks: {
-          maxRotation: 90,
-          minRotation: 80
         }
-      }],
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
+        else {
+            otherCommits += e.commits;
         }
-      }]
+        
+    });
+
+
+    console.log(numbers);
+    if(numbers.length == 8){
+    var config = {
+        type: 'bar',
+        data: {
+            datasets: [{
+                borderWidth: 1,
+                data: numbers,
+                label: data.rg_name,
+                backgroundColor: bgColors,
+                borderColor: borderColors
+            }],
+            labels: emails
+        },
+        options: {
+            responsive: true,
+				legend: {
+					display: false,
+				},
+				title: {
+					display: true,
+					text: 'Top Contributors for Each Repo Group'
+				},
+				animation: {
+					animateScale: true,
+					animateRotate: true
+				}
+        }
+    };
+    
+    var ctx = document.getElementById("canvas");
+    window.myChart = new Chart(ctx, config);
     }
-  }
-});
 }
